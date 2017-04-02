ffe64484b3ae40bcc396ce31a0d953cb5c78aa90
Williams Medina <williams.medinaa@gmail.com>
2017-04-02 10:57:55 -0400

    chore: methods and events for runtime domain

--- lib/domain/runtime.d.ts
+++ lib/domain/runtime.d.ts
@@ -1,8 +1,72 @@
 import { ChromeDebuggingDomain } from '../chrome-debugging-domain';
 export declare class RuntimeDomain extends ChromeDebuggingDomain {
-    enable(): Promise<{}>;
+    evaluate(params: {
+        expression: string;
+        objectGroup?: string;
+        includeCommandLineAPI?: boolean;
+        silent?: boolean;
+        contextId?: boolean;
+        returnByValue?: boolean;
+        generatePreview?: boolean;
+        userGesture?: boolean;
+        awaitPromise?: boolean;
+    }): Promise<{}>;
+    awaitPromise(params: {
+        promiseObjectId: string;
+        returnByValue?: boolean;
+        generatePreview?: boolean;
+    }): Promise<{}>;
+    callFunctionOn(params: {
+        objectId: string;
+        functionDeclaration: string;
+        arguments?: Array<{
+            value?: string;
+            unserializableValue?: string;
+            objectId?: string;
+        }>;
+        silent?: boolean;
+        returnByValue?: boolean;
+        generatePreview?: boolean;
+        userGesture?: boolean;
+        awaitPromise?: boolean;
+    }): Promise<{}>;
+    getProperties(params: {
+        objectId: string;
+        ownProperties?: boolean;
+        accessorPropertiesOnly?: boolean;
+        generatePreview?: boolean;
+    }): Promise<{}>;
+    releaseObject(params: {
+        objectId: string;
+    }): Promise<{}>;
+    releaseObjectGroup(params: {
+        objectGroup: string;
+    }): Promise<{}>;
     runIfWaitingForDebugger(): Promise<{}>;
-    getProperties(options: any): Promise<{}>;
+    enable(): Promise<{}>;
+    disable(): Promise<{}>;
+    discardConsoleEntries(): Promise<{}>;
+    compileScript(params: {
+        expression: string;
+        sourceURL: string;
+        persistScript: boolean;
+        executionContextId?: number;
+    }): Promise<{}>;
+    runScript(params: {
+        scriptId: string;
+        executionContextId?: string;
+        objectGroup?: string;
+        silent?: boolean;
+        includeCommandLineAPI?: boolean;
+        returnByValue?: boolean;
+        generatePreview?: boolean;
+        awaitPromise?: boolean;
+    }): Promise<{}>;
+    executionContextCreated(cb: Function): this;
+    executionContextDestroyed(cb: Function): this;
+    executionContextsCleared(cb: Function): this;
     exceptionThrown(cb: Function): this;
+    exceptionRevoked(cb: Function): this;
     consoleAPICalled(cb: Function): this;
+    inspectRequested(cb: Function): this;
 }
--- src/domain/runtime.ts
+++ src/domain/runtime.ts
@@ -2,20 +2,112 @@ import { ChromeDebuggingDomain } from '../chrome-debugging-domain'
 
 export class RuntimeDomain extends ChromeDebuggingDomain {
   // methods
-  enable () {
-    return this.send('enable')
+  evaluate (params: {
+    expression: string,
+    objectGroup?: string,
+    includeCommandLineAPI?: boolean,
+    silent?: boolean,
+    contextId?: boolean,
+    returnByValue?: boolean,
+    generatePreview?: boolean,
+    userGesture?: boolean,
+    awaitPromise?: boolean
+  }) {
+    return this.send('evaluate', params)
+  }
+  awaitPromise (params: {
+    promiseObjectId: string,
+    returnByValue?: boolean,
+    generatePreview?: boolean
+  }) {
+    return this.send('awaitPromise', params)
+  }
+  callFunctionOn (params: {
+    objectId: string,
+    functionDeclaration: string,
+    arguments?: Array<{
+      value?: string,
+      unserializableValue?: string,
+      objectId?: string
+    }>,
+    silent?: boolean,
+    returnByValue?: boolean,
+    generatePreview?: boolean,
+    userGesture?: boolean,
+    awaitPromise?: boolean
+  }) {
+    return this.send('callFunctionOn', params)
+  }
+  getProperties (params: {
+    objectId: string,
+    ownProperties?: boolean,
+    accessorPropertiesOnly?: boolean,
+    generatePreview?: boolean
+  }) {
+    return this.send('getProperties', params)
+  }
+  releaseObject (params: {
+    objectId: string
+  }) {
+    return this.send('releaseObject', params)
+  }
+  releaseObjectGroup (params: {
+    objectGroup: string
+  }) {
+    return this.send('releaseObjectGroup', params)
   }
   runIfWaitingForDebugger () {
     return this.send('runIfWaitingForDebugger')
   }
-  getProperties (options) {
-    return this.send('getProperties', options)
+  enable () {
+    return this.send('enable')
+  }
+  disable () {
+    return this.send('disable')
+  }
+  discardConsoleEntries () {
+    return this.send('discardConsoleEntries')
+  }
+  compileScript (params: {
+    expression: string,
+    sourceURL: string,
+    persistScript: boolean,
+    executionContextId?: number
+  }) {
+    return this.send('compileScript', params)
+  }
+  runScript (params: {
+    scriptId: string,
+    executionContextId?: string,
+    objectGroup?: string,
+    silent?: boolean,
+    includeCommandLineAPI?: boolean,
+    returnByValue?: boolean,
+    generatePreview?: boolean,
+    awaitPromise?: boolean
+  }) {
+    return this.send('runScript', params)
   }
   // events
+  executionContextCreated (cb: Function) {
+    return this.addListener('executionContextCreated', cb)
+  }
+  executionContextDestroyed (cb: Function) {
+    return this.addListener('executionContextDestroyed', cb)
+  }
+  executionContextsCleared (cb: Function) {
+    return this.addListener('executionContextsCleared', cb)
+  }
   exceptionThrown (cb: Function) {
     return this.addListener('exceptionThrown', cb)
   }
+  exceptionRevoked (cb: Function) {
+    return this.addListener('exceptionRevoked', cb)
+  }
   consoleAPICalled (cb: Function) {
     return this.addListener('consoleAPICalled', cb)
+  }
+  inspectRequested (cb: Function) {
+    return this.addListener('inspectRequested', cb)
   }
 }
