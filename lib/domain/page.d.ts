d0db5417c8c803b293abc73f612413a6690cf83b
Williams Medina <williams.medinaa@gmail.com>
2017-04-02 10:38:12 -0400

    complete page domain events and methods

--- lib/domain/page.d.ts
+++ lib/domain/page.d.ts
@@ -1,4 +1,26 @@
 import { ChromeDebuggingDomain } from '../chrome-debugging-domain';
 export declare class PageDomain extends ChromeDebuggingDomain {
-    navigate(params: Object): Promise<{}>;
+    enable(): Promise<{}>;
+    disable(): Promise<{}>;
+    reload(params: {
+        ignoreCache?: boolean;
+        scriptToEvaluateOnLoad?: string;
+    }): Promise<{}>;
+    navigate(params: {
+        url: string;
+    }): Promise<{}>;
+    handleJavaScriptDialog(params: {
+        accept: boolean;
+        promptText?: string;
+    }): Promise<{}>;
+    domContentEventFired(cb: Function): this;
+    loadEventFired(cb: Function): this;
+    frameAttached(cb: Function): this;
+    frameNavigated(cb: Function): this;
+    frameDetached(cb: Function): this;
+    javascriptDialogOpening(cb: Function): this;
+    javascriptDialogClosed(cb: Function): this;
+    interstitialShown(cb: Function): this;
+    interstitialHidden(cb: Function): this;
+    navigationRequested(cb: Function): this;
 }
--- src/domain/page.ts
+++ src/domain/page.ts
@@ -2,7 +2,58 @@ import { ChromeDebuggingDomain } from '../chrome-debugging-domain'
 
 export class PageDomain extends ChromeDebuggingDomain {
   // methods
-  navigate (params: Object) {
+  enable () {
+    return this.send('enable')
+  }
+  disable () {
+    return this.send('disable')
+  }
+  reload (params: {
+    ignoreCache?: boolean,
+    scriptToEvaluateOnLoad?: string
+  }) {
+    return this.send('reload', params)
+  }
+  navigate (params: {
+    url: string
+  }) {
     return this.send('navigate', params)
+  }
+  handleJavaScriptDialog (params: {
+    accept: boolean,
+    promptText?: string
+  }) {
+    return this.send('handleJavaScriptDialog', params)
+  }
+  // events
+  domContentEventFired (cb: Function) {
+    return this.addListener('domContentEventFired', cb)
+  }
+  loadEventFired (cb: Function) {
+    return this.addListener('loadEventFired', cb)
+  }
+  frameAttached (cb: Function) {
+    return this.addListener('frameAttached', cb)
+  }
+  frameNavigated (cb: Function) {
+    return this.addListener('frameNavigated', cb)
+  }
+  frameDetached (cb: Function) {
+    return this.addListener('frameDetached', cb)
+  }
+  javascriptDialogOpening (cb: Function) {
+    return this.addListener('javascriptDialogOpening', cb)
+  }
+  javascriptDialogClosed (cb: Function) {
+    return this.addListener('javascriptDialogClosed', cb)
+  }
+  interstitialShown (cb: Function) {
+    return this.addListener('interstitialShown', cb)
+  }
+  interstitialHidden (cb: Function) {
+    return this.addListener('interstitialHidden', cb)
+  }
+  navigationRequested (cb: Function) {
+    return this.addListener('navigationRequested', cb)
   }
 }
